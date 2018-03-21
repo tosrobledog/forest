9337ce313c0f650c1e183acd5551d1916facea73
Alejandro Hernandez Valencia <alejoskygear@gmail.com>
2018-03-21 13:10:08 -0500

    Update Read_Files_Scopus.R

--- Read_Files_Scopus.R
+++ Read_Files_Scopus.R
@@ -1,4 +1,6 @@
-library(bibliometrix)   ### load bibliometrix package
+# load bibliometrix package
+library(bibliometrix)
 
+# Scopus source file should be named 'scopus.bib'
 Documents <- readFiles("scopus.bib")
 M <- convert2df(Documents, dbsource = "scopus", format = "bibtex")
