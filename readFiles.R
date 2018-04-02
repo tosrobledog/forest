df4b35989a8a7087e8e96ed76cd7ec0118a0182f
srobledog <srobledog@unal.edu.co>
2018-04-02 12:29:57 -0500

    Creating datasets and entities

--- readFiles.R
+++ readFiles.R
@@ -1,5 +1,7 @@
 library(bibliometrix)
-
+library(DBI)
+library(RS)
+library(RSQLite)
 
 readISI <- function(...){
   D <- readFiles(...)
@@ -12,3 +14,34 @@ readScopus <- function(...) {
   M <- convert2df(Documents, dbsource = "scopus", format = "bibtex")
   M
 }
+
+# Creating forest database
+
+forest <- dbConnect(RSQLite::SQLite(), "forest.sqlite")
+dbDisconnect(forest)
+unlink("forest.sqlite")
+
+
+
+## Getting Data
+
+wos.data <- readISI("data/ISI/savedrecs.txt")
+
+## Creting Entities
+### Creating paper entity
+
+paper <- wos.data[,c("SR", "TI", "PY", "VL", "DI", "AF", "AB", "DE", "SO", "DT")]
+names(paper) <- c("id", "title", "year", "volume", "doi", "affilitation", "abstract",
+                  "keywords", "publisher", "document_type")
+
+
+# Loading data into forest database
+
+dbWriteTable(forest, "Paper", paper)
+dbWriteTable(forest, "ReferenceLink", references)
+dbWriteTable(forest, "PaperAuthor", paperauthor)
+dbWriteTable(forest, "Author", author)
+dbWriteTable(forest, "PaperJournal", paperjournal)
+dbWriteTable(forest, "Journal", journal)
+
+
