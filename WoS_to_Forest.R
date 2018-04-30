e41fde6f7cc294370dbe8e5474be55d2fdc714fc
manuelpt4940 <mperezt@unal.edu.co>
2018-04-30 21:52:17 +0000

    Some modifications

--- WoS_to_Forest.R
+++ WoS_to_Forest.R
@@ -141,20 +141,29 @@ split2.df <- data.frame(gender=character())
 options(stringsAsFactors = FALSE)
 for (i in 1:length(split1))
 {
-        # split2<-strsplit(split1[[i]][2]," ")
+        ###Se escoge el nombre del autor, y se desecha el apellido del mismo
+        # split2<-strsplit(split1[[i]][2]," ")  
         # #print(gender(split2[[1]][1]))
+        
+        ###Se escoge el primer nombre, ya que varios autores tienen dos nombres, se le 
+        ###aplica el gender y se convierte en dataframe para obtener el género fácilmente
         # gen1<-data.frame(gender(split2[[1]][1]))
         # #genero<-gen1[1,4]
+        
+        ###Se selecciona la ubicación en donde aparece el género, y con rbind se van guardando
+        ###uno debajo del otro en un dataframe
         # split2.df<-rbind(split2.df,gen1[1,4])
         # #split2.df<-rbind(split2.df,split2[[1]][1])
         split2<-strsplit(split1[[i]][2]," ")
         tryCatch({
-                gen1<-data.frame(findGivenNames(split2[[1]][1][57:NROW(xPrepared)], progress=FALSE))
+                gen1<-data.frame(findGivenNames(split2[[1]][1], progress=FALSE))
         }, error=function(e){})
         split2.df<-rbind(split2.df,gen1[1,2])
 }
 #names(split2.df) <- "Gender"
 names(split2.df) <- "GenderizeR"
+
+###Se unifica el dataframe de autores con el de género para que quede en uno solo
 author<-cbind(author,split2.df)
 ######
 
