93817dc60dbac2ca544478c4619affd50499ee28
tosrobledog <sebastian.robledo@gmail.com>
2018-05-25 14:17:55 +0000

    added some letters

--- shinyapp_2/app_3.R
+++ shinyapp_2/app_3.R
@@ -26,7 +26,7 @@ ui <- dashboardPage(
       
       # Second tab content
       tabItem(tabName = "widgets",
-              h2("Widgets tab content")
+              h2("Widgets tab content lo que quieras escribir")
       )
     )
   )
